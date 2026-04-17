47c5f305b9677f28ce0fe7897a08b97eb356ebbb
Moritz Béla Meier <moritz@moritxius.de>
2026-04-17 15:12:34 +0200

    Update route.ts

--- src/app/api/return/route.ts
+++ src/app/api/return/route.ts
@@ -4,45 +4,7 @@ import { RateLimit } from "@/lib/rate-limit";
 import { prisma } from "@/lib/prisma";
 
 export async function DELETE(request: NextRequest) {
-	const session = null;
-	if (!session) return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-
-	const rateLimit = new RateLimit(request, 1);
-	const check = await rateLimit.handle();
-	if (check) return check;
-
-	const activePunishment = await prisma.punishment.findFirst({
-		where: {
-			userId: Number(session.user?.id),
-			returned: false,
-		},
-		include: {
-			violatingMiis: {
-				include: {
-					mii: {
-						select: {
-							name: true,
-						},
-					},
-				},
-			},
-		},
-	});
-
-	if (!activePunishment) return rateLimit.sendResponse({ error: "You have no active punishments!" }, 404);
-	if (activePunishment.type === "PERM_EXILE") return rateLimit.sendResponse({ error: "Your punishment is permanent" }, 403);
-	if (activePunishment.type === "TEMP_EXILE" && activePunishment.expiresAt! > new Date())
-		return rateLimit.sendResponse({ error: "Your punishment has not expired yet." }, 403);
-
-	await prisma.punishment.update({
-		where: {
-			id: activePunishment.id,
-		},
-		data: {
-			returned: true,
-		},
-	});
-
-	return rateLimit.sendResponse({ success: true });
+	// Authentication has been removed - deny all requests
+	return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
 }
 
